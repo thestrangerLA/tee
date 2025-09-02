e718e70d63a619f51df5b8d0c1e32beb967b5143
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2025-09-02 11:00:15 +0000

    check again to complete

--- src/services/transportService.ts
+++ src/services/transportService.ts
@@ -21,7 +21,7 @@ const transportCollectionRef = collection(db, 'transportEntries');
 
 const createInitialRowState = (type: 'ANS' | 'HAL' | 'MX', date: Date): Omit<TransportEntry, 'id' | 'createdAt'> => ({
     type: type,
-    date: startOfDay(date), // Default to today
+    date: startOfDay(date),
     detail: '',
     cost: 0,
     amount: 0,
