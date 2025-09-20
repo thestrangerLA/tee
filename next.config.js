6cc7710e2713c4a53208719543c12ed821d8c698
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2025-09-20 09:10:50 +0000

    File changes

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
