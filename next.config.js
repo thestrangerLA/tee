d6c6acfb6ee953922f63d47056c698fd74697d04
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2025-09-20 10:28:37 +0000

    ปัญหาที่ยังคงอยู่: next export ยังคงทำงาน:
    Log แสดง "detected next expo

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
